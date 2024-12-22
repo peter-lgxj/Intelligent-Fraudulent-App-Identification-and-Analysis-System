18377ec982e568b2e54b0a767ceb610ba3ac4492
Peter_X <125550169+peter-lgxj@users.noreply.github.com>
2024-12-22 13:40:55 +0800

    Update server.py

--- server.py
+++ server.py
@@ -135,4 +135,4 @@ def download_file():
         abort(404)
         
 if __name__ == '__main__':
-    socketio.run(app,host='127.0.0.1',port=5000, debug=True)
+    socketio.run(app,host='127.0.0.1',port=5000, debug=False)
